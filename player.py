a4c10d9ed871eddcd5ac472fc642728155412bcf
xnaizer <seykig028@gmail.com>
2026-05-15 17:30:10 +0700

    (feat: player): limiting shoot

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 import pygame 
 from shot import Shot
 
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_cooldown_count = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -26,6 +27,7 @@ class Player(CircleShape):
 
     def update(self, dt): 
         keys = pygame.key.get_pressed()
+        self.shoot_cooldown_count -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -40,13 +42,17 @@ class Player(CircleShape):
             self.move(-dt)
 
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shoot_cooldown_count <= 0:
+                self.shoot()
+                self.shoot_cooldown_count = PLAYER_SHOOT_COOLDOWN_SECONDS
+            
+            
     
     def move(self, dt):
         unit_vector = pygame.Vector2(0,1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
-        self.position += rotated_with_speed_vector
+        self.position += rotated_with_speed_vector 
 
     def shoot(self):
         shot_data = Shot(self.position.x, self.position.y)
